b76b7d08ec45151fb0a11c97b945383871b42399
SkooGamer <skoogamerofficial@gmail.com>
2025-02-08 21:02:54 -0300

    update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,24 +1,17 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export_subgroup("Movement")
+@export var speed = 300.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# Obtém a direção com base na entrada do jogador
+	var direction := Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("backward") - Input.get_action_strength("forward")
+	).normalized() # Normaliza para manter a velocidade constante em diagonal
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Define a velocidade com base na direção
+	velocity = direction * speed
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Move o personagem
 	move_and_slide()
